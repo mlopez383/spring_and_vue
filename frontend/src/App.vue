507c5efd8677ab1fc7895ac9c394f2958d8ee608
Miguel Lopez <miguel.lopez@avantica.com>
2024-06-11 13:39:31 -0400

    upload vue and spring boot app

---
+++ frontend/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="/">
+        CRUD with Spring Boot, Vue.js, and PostgreSQL
+        (updated: Miguel Lopez)
+        </a><br/><br/>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "app"
+};
+</script>
+
+<style>
+@import 
+url(https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css);
+
+</style>
